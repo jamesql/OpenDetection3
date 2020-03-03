4f9a796bce7be48c7f60f962e8bf8168e6e7fb49
smitteh1 <thejamesash1@gmail.com>
2020-03-03 18:41:08 -0500

    added example and starting to work on python classes

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,10 @@
+import os.path
 import cv2
 
-image = cv2.imread("../images/hand1.png");
-
+# simple finger contours testing 
 def main() :
+    image = cv2.imread(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/images/hand1.png');
     cv2.imshow("test", image);
-    cv2.waitKey(30);
-    return 0;
+    cv2.waitKey(0);
 
 main();
---
+++ examples/python/displayimage.py
@@ -0,0 +1,13 @@
+import ODImage
+
+def main() :
+    # contructor requires a path.
+    image = ODImage("image1.png");
+
+    # display image
+    image.display();
+    # you can also display it with a window name
+    # image.display("window_name");
+
+
+main();
