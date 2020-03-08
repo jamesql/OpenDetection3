e6a814a21f8579b43ae22f3d7de8a82520a8c9be
smitteh1 <thejamesash1@gmail.com>
2020-03-08 10:33:43 -0400

    add more objects

---
+++ Python/Filter.py
@@ -0,0 +1,3 @@
+import cv2
+from Image import ODImage
+from Video import ODVideo
---
+++ Python/Morph.py
@@ -0,0 +1,8 @@
+import cv2
+from Image import ODImage
+from Video import ODVideo
+from Filter import ODFilter
+from Exception import ODException
+
+class ODMorph:
+    def __init__(self, frame):
