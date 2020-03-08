553fad33ff0d80508f29035fc8b22c9189e9a18b
smitteh1 <thejamesash1@gmail.com>
2020-03-08 13:10:39 -0400

    add impots and recon class

--- Python/OpenDetection.py
+++ Python/OpenDetection.py
@@ -1,2 +1,5 @@
 from Image import ODImage
 from Exception import ODException
+from Sensor import ODSensor
+from Recon import ODRecon
+from Morph import ODMorph
---
+++ Python/Recon.py
@@ -0,0 +1,4 @@
+import cv2
+
+class ODRecon:
+    def __init__(self):
